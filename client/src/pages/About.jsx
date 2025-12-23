65825431d0739c2b7a5f140564ed5f5507475075
Žikica Drača <zikica.draca@gmail.com>
2025-12-23 18:05:43 +0100

    added-lazy-about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,21 @@
+import { Suspense, lazy } from 'react';
 import AboutUs from '../components/about/aboutUsSec/AboutUs';
-import OurVision from '../components/about/aboutUsSec/OurVision';
-import OurSkills from '../components/about/aboutUsSec/OurSkills';
+// import OurVision from '../components/about/aboutUsSec/OurVision';
+// import OurSkills from '../components/about/aboutUsSec/OurSkills';
 import Seo from '../components/SEO/Seo';
 import seoData from '../seo/seoData.json';
 import { useTranslation } from 'react-i18next';
+
+const OurVision = lazy(() =>
+  import(/* webpackPrefetch: true */ '../components/about/aboutUsSec/OurVision')
+);
+const OurSkills = lazy(() =>
+  import(/* webpackPrefetch: true */ '../components/about/aboutUsSec/OurSkills')
+);
+
+const Loading = () => {
+  return <p>Loading...</p>;
+};
 
 const About = () => {
   const { i18n } = useTranslation();
@@ -21,8 +33,10 @@ const About = () => {
       />
       <main>
         <AboutUs />
-        <OurVision />
-        <OurSkills />
+        <Suspense fallback={<Loading />}>
+          <OurVision />
+          <OurSkills />
+        </Suspense>
       </main>
     </>
   );
