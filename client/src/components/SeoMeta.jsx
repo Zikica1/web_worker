ebb59ebad7474d1f51121426dae46099aeb3f313
Žikica Drača <zikica.draca@gmail.com>
2026-01-28 18:32:08 +0100

    added-metadata-services

---
+++ client/src/components/SeoMeta.jsx
@@ -0,0 +1,51 @@
+import AlternateLinks from './SEO/AlternateLinks';
+
+const SeoMeta = ({
+  title,
+  description,
+  url,
+  alternates,
+  canonical,
+  image,
+  jsonLd,
+  type = 'website',
+}) => {
+  return (
+    <>
+      <title>{title}</title>
+
+      {canonical && <link rel='canonical' href={canonical} />}
+
+      <meta name='description' content={description} />
+      <meta name='robots' content='index, follow' />
+
+      {/* Open Graph */}
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      {image && <meta property='og:image' content={image} />}
+      {image && <meta property='og:image:width' content='1200' />}
+      {image && <meta property='og:image:height' content='630' />}
+      {(canonical || url) && (
+        <meta property='og:url' content={canonical || url} />
+      )}
+      <meta property='og:type' content={type} />
+
+      {/* Twitter */}
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
+      {image && <meta name='twitter:image' content={image} />}
+
+      {/* hreflang */}
+      {alternates?.map(({ href, lang }) => (
+        <link key={lang} rel='alternate' href={href} hrefLang={lang} />
+      ))}
+
+      {jsonLd && (
+        <script type='application/ld+json'>{JSON.stringify(jsonLd)}</script>
+      )}
+    </>
+  );
+};
+
+export default SeoMeta;
--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,23 +1,40 @@
 import Service from '../components/services/Service';
 import ServiceTeamEffort from '../components/services/ServiceTeamEffort';
-import Seo from '../components/SEO/Seo';
 import seoData from '../seo/seoData.json';
 import { useTranslation } from 'react-i18next';
+import SeoMeta from '../components/SeoMeta';
 
 const Services = () => {
   const { i18n } = useTranslation();
   const lang = i18n.language;
   const { title, description, url, image, jsonLd } = seoData.services[lang];
 
+  const alternates = [
+    {
+      lang: 'sr',
+      href: seoData.services.sr.url,
+    },
+    {
+      lang: 'en',
+      href: seoData.services.en.url,
+    },
+    {
+      lang: 'x-default',
+      href: seoData.services.sr.url,
+    },
+  ];
+
   return (
     <>
-      <Seo
+      <SeoMeta
         title={title}
         description={description}
         url={url}
+        alternates={alternates}
         canonical={url}
         image={image}
         jsonLd={jsonLd}
+        type='website'
       />
       <main className='services'>
         <Service />
